09cae8b077851f5c71fe1b63cfbb42b8c8237c42
MaximilianoRom7 <maximilianoromayfigueroa@hotmail.com>
2018-04-22 16:11:38 -0300

    now the Square value is controlled by the Board component

--- src/index.js
+++ src/index.js
@@ -5,30 +5,74 @@ import './index.css';
 
 class Square extends React.Component {
     constructor(props) {
+        /*
+         * this is constructor is called once even
+         * when the prop value changes
+        */
         super(props);
         this.state = {
-            value: null
+            value: null,
         };
     }
 
     render() {
+        /*
+          Never change the state in render because render depends
+          on the state making this an never ending recursive function
+
+          if(this.props.value)
+              this.setState({value: this.props.value});
+        */
+        var value;
+        if(this.props.onClick)
+            value = this.props.value;
+        else
+            value = this.state.value;
         return (
             <button className="square" onClick={this.onClick.bind(this)}>
-              {this.state.value}
+              {value}
             </button>
         );
     }
 
     /* set the square state on the click event */
     onClick(event) {
-        this.setState({value: 'X'});
-        console.log('Square Clicked: ' + this.props.value);
+        if(this.props.onClick) {
+            /* delegate state change to the upper component in this case Board */
+            this.props.onClick(event);
+            console.log("Delegate onClick");
+        }
+        else {
+            /* default state change */
+            this.setState({value: 'X'});
+        }
+        console.log('Square Clicked: ' + this.props.ID);
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        /* Board saves the state of each Square */
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    onSquareClick(squareID) {
+        /* copy array to prevent mutability */
+        const squares = this.state.squares.slice();
+        squares[squareID] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return (
+            <Square
+              ID={i}
+              value={this.state.squares[i]}
+              onClick={(event) => this.onSquareClick(i)}/>
+        );
     }
 
     render() {
